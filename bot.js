e3213c651885c6408a1ef832954beda11a65efe8
StanleyTack <stanley.tack@gmail.com>
2016-07-11 21:41:15 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = "wifi";
+  botResponse = "Wifi details";
 
   options = {
     hostname: 'api.groupme.com',
